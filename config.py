4b4bbcfa4c1bc0ddaad1ef7615d1ee1f0fa6c3f5
David Sergeievich <122042891+Matiosov@users.noreply.github.com>
2023-02-02 14:17:59 +0300

    Добавлен файл  config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+
+TOKEN = '6128941904:AAGq1KNwrfH0P8wZZZowRdk_BqhRKPu1fZo'
+
+keys = {'евро': 'EUR', 'рубль': 'RUR', 'доллар': 'USD'}
+
+
+TIMEZONE = 'Rostov on Don'
+TIMEZONE_COMMON_NAME = 'Rostov on Don'
+
+
