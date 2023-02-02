978b9b22f95f22f42fab918f6ff6975eef6b764a
David Sergeievich <122042891+Matiosov@users.noreply.github.com>
2023-02-02 15:12:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import requests
+import json
 
 TOKEN = '6128941904:AAGq1KNwrfH0P8wZZZowRdk_BqhRKPu1fZo'
 
@@ -6,5 +8,3 @@ keys = {'евро': 'EUR', 'рубль': 'RUR', 'доллар': 'USD'}
 
 TIMEZONE = 'Rostov on Don'
 TIMEZONE_COMMON_NAME = 'Rostov on Don'
-
-
