953f7e177728a00c84330b7903d56e8cc7f0eebb
David Sergeievich <122042891+Matiosov@users.noreply.github.com>
2023-02-04 10:13:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
-import requests
-import json
 
 TOKEN = '6128941904:AAGq1KNwrfH0P8wZZZowRdk_BqhRKPu1fZo'
 
-keys = {'евро': 'EUR', 'рубль': 'RUR', 'доллар': 'USD'}
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUR',
+    'евро': 'EUR'
+}
 
 
 TIMEZONE = 'Rostov on Don'
