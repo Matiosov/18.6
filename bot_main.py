08768ae145367ed9a528d17fcf9445384411cbec
David Sergeievich <122042891+Matiosov@users.noreply.github.com>
2023-02-04 10:17:33 +0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -1,46 +1,41 @@
 import telebot
 from config import keys, TOKEN
-from extensions import ConvertionException, CryptoConverter
+from extensions import APIException, CryptoConverter
 
-bot = telebot.TeleBot(TOKEN, parse_mode=None)
+bot = telebot.TeleBot(TOKEN)
 
-
-# обработчик команд [старт, хелп]
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
-    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> ' \
-           '<в какую валюту перевести> ' \
-           '<количество переводимой валюты>'
+    text = 'Приветствую! Я бот-конвертер. Введите команду в формате:\n <Название валюты(какую)> \
+ <Название валюты(в какую)> <Количество переводимой валюты> \n Показать список доступной валюты: /values'
     bot.reply_to(message, text)
 
 
-# Обработчик команды [values]
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in keys.keys():
-        text = '\n'.join((text, key,))
+        text = '\n'.join((text, key, ))
     bot.reply_to(message, text)
 
 
-# Обработчик текста
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text', 1])
 def convert(message: telebot.types.Message):
     try:
-        values = message.text.lower().split(' ')
+        values = message.text.split(' ')
 
         if len(values) != 3:
-            raise ConvertionException('Не верное количество параметров.')
+            raise APIException('Количество параметров не совпадает с форматом ввода.\n')
 
         quote, base, amount = values
-        total_base = CryptoConverter.get_price(quote, base, amount)
-    except ConvertionException as e:
-        bot.reply_to(message, f'Ошибка пользователя\n{e}')
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя. \n{e} Как надо?: /help')
     except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+        bot.reply_to(message, f'Не удалось обработать команду\n{e} \nКак правильно вводить данные: /help')
     else:
         text = f'Цена {amount} {quote} в {base} - {total_base}'
         bot.send_message(message.chat.id, text)
 
 
-bot.infinity_polling()
+bot.polling()
